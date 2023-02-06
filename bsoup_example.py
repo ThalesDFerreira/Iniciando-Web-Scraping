c1b45e28655752a4dd52318834ddd45cd9ea45e4
ThalesFerreira <thalesferreira190@gmail.com>
2023-02-06 05:58:05 -0300

    aprendendo sobre o beautifulsoup4-finalizado

--- bsoup_example.py
+++ bsoup_example.py
@@ -5,9 +5,24 @@ url = "https://quotes.toscrape.com"
 page = requests.get(url)
 html_content = page.text
 
-# Cria uma instância do objeto Beautiful Soup e usa o parser de HTML nativo
-# do Python
 soup = BeautifulSoup(html_content, "html.parser")
 
-# Utiliza o método prettify para melhorar a visualização do conteúdo
-print(soup.prettify())
+print(soup)
+
+# acessando a tag 'title'
+title = soup.title
+
+# retorna o elemento HTML da tag
+print(title)
+
+# o tipo de 'title' é tag
+print(type(title))
+
+# o nome de 'title' é title
+print(title.name)
+
+# acessando a tag 'footer'
+footer = soup.footer
+
+# acessando o atributo classe da tag footer
+print(footer['class'])
